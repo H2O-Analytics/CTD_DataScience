cf13257bb3624c6d52a58f643650cf87a8050806
Taylor Waters <waterstaylor80@hotmail.com>
2022-09-04 07:59:37 -0400

    creating a titianic exploratory program file for first attempt.

---
+++ Kaggle/programs/titanic_data_explore.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+
+# Data input
+DATA_PATH_TW = "/Users/tawate/Library/CloudStorage/OneDrive-SAS/CDT_DataScience_Drive/kaggle_ds/titanic/"
+train_df = pd.read_csv(DATA_PATH_TW + "train.csv")
+test_df = pd.read_csv(DATA_PATH_TW + "test.csv")
+
+# Exploratory Analysis
+
+# Random Forest Prediction
